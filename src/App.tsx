12e0d6dc305ba1c0cd3a2642b905e5e10d1c6037
dangthaii <dangthaiuseful@gmail.com>
2023-04-24 16:53:10 +0700

    build layout of website

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 function App() {
   return (
-    <>
-      <Button colorScheme="orange" variant="outline">
-        Hello
-      </Button>
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "sidebar main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        navbar
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="sidebar" bg="gray">
+          sidebar
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="pink">
+        main website
+      </GridItem>
+    </Grid>
   );
 }
 
