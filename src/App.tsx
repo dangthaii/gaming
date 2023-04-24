1939929c1c7da6e2b79c3228f7f6964e3c0b8e6a
dangthaii <dangthaiuseful@gmail.com>
2023-04-24 16:19:30 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@chakra-ui/react";
+function App() {
+  return (
+    <>
+      <Button colorScheme="orange" variant="outline">
+        Hello
+      </Button>
+    </>
+  );
+}
+
+export default App;
